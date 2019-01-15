5ce8b2d3c42f086d1d1997b9ad3da6f4fecfd060
The Stygian Architect <thestygianarchitect@gmail.com>
2019-01-15 00:21:11 -0500

    removed python 2 compatibility
    Signed-off-by: The Stygian Architect <thestygianarchitect@gmail.com>

--- tests/context.py
+++ tests/context.py
@@ -6,10 +6,8 @@
     Description: This module adds context to testing suite
 """
 
-# import os
 import sys
 from pathlib import Path
-# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 sys.path.insert(0, Path(__file__).absolute().parent)  # Python 3 only
 
 import app
